4278fafb50909257e19b712b045fedbf4215b46a
jh <jh@jhdeiMac.local>
2026-02-05 13:39:27 +0800

    server: refine enable-login endpoint

--- server/api/enable-login.ts
+++ server/api/enable-login.ts
@@ -3,6 +3,15 @@ import process from "node:process"
 import { defineEventHandler } from "h3"
 
 export default defineEventHandler(async () => {
+  const required = ["JWT_SECRET", "G_CLIENT_ID", "G_CLIENT_SECRET"] as const
+  const missing = required.filter(k => !process.env[k])
+  if (missing.length) {
+    return {
+      enable: false,
+      missing,
+    }
+  }
+
   return {
     enable: true,
     url: `https://github.com/login/oauth/authorize?client_id=${process.env.G_CLIENT_ID}`,
